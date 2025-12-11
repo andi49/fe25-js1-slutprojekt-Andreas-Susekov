5ffc7411cae03c844ccbce0fdefcb445aca00797
andi49 <46962871+andi49@users.noreply.github.com>
2025-12-11 21:17:45 +0100

    Update Start

---
+++ js/api.js
@@ -0,0 +1,40 @@
+// Heter Access Token Auth i TMDB
+
+export async function getData() {
+    
+
+const BAERER_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MmVkOTkxMzRhYWYxZjBiYTBhZmU0ZWEwZmFjMDdhNiIsIm5iZiI6MTc2NTM2NDYyMS4yNDgsInN1YiI6IjY5Mzk1MzhkNDAyMmI4MTExZWFmMDY3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8ZJyuuRfGvaFqtxi6kGMaqCVxS4A09pr-M9-C3Ib1TI';
+
+const url = 'https://api.themoviedb.org/3/discover/movie';
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${BAERER_KEY}`
+  }
+};
+
+try {
+    const res = await fetch(url, options)
+
+    if(!res.ok) {
+        throw new Error(`HTTP error: ${res.status}`)
+    }
+
+    const data = await res.json()
+  return (data)
+
+} catch(err) {
+    console.error('Data error', err)
+    return null
+}
+}
+ 
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
+
+//   
